0353ec8bc9e3c1c37698b6e3c9a81c1e2e061cc5
Vlad <19vlad_son90@mail.ru>
2016-05-30 13:56:41 +0300

    JavaRushHomeWork

--- src/com/javarush/test/level19/lesson10/bonus02/FileConsoleWriter.java
+++ src/com/javarush/test/level19/lesson10/bonus02/FileConsoleWriter.java
@@ -6,68 +6,55 @@ package com.javarush.test.level19.lesson10.bonus02;
 При записи данных в файл, должен дублировать эти данные на консоль
 */
 
-
+import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 
+public class FileConsoleWriter extends FileWriter {
 
-public class FileConsoleWriter extends FileWriter
-{
-    private FileWriter fileWriter;
-
-    public FileConsoleWriter (String fileName) throws IOException
-    {
+    public FileConsoleWriter(String fileName) throws IOException {
         super(fileName);
-        this.fileWriter = new FileWriter(fileName);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        super(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        super(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        super(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        super(fd);
     }
 
     @Override
-    public void write(int c) throws IOException
-    {
+    public void write(int c) throws IOException {
         super.write(c);
+        char[] chars = new char[]{(char) c};
+        for (char i : chars)
+            System.out.print(i);
+        System.out.println();
     }
 
     @Override
-    public void write(char[] cbuf) throws IOException
-    {
-        super.write(cbuf);
-    }
-
-    @Override
-    public void write(char[] cbuf, int off, int len) throws IOException
-    {
+    public void write(char[] cbuf, int off, int len) throws IOException {
         super.write(cbuf, off, len);
+        for (int i = 0; i < len; i++)
+            System.out.print(cbuf[i + off]);
+        System.out.println();
     }
 
     @Override
-    public void write(String str) throws IOException
-    {
-        super.write(str);
-    }
-
-    @Override
-    public void write(String str, int off, int len) throws IOException
-    {
+    public void write(String str, int off, int len) throws IOException {
         super.write(str, off, len);
+        for (int i = 0; i < len; i++)
+            System.out.print(str.toCharArray()[i + off]);
+        System.out.println();
     }
-
-    @Override
-    public Writer append(char c) throws IOException
-    {
-        return super.append(c);
-    }
-
-    @Override
-    public Writer append(CharSequence csq) throws IOException
-    {
-        return super.append(csq);
-    }
-
-    @Override
-    public Writer append(CharSequence csq, int start, int end) throws IOException
-    {
-        return super.append(csq, start, end);
-    }
-
 }
