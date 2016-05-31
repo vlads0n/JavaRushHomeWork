97246ea43374d6531fb43a50f8d2cc8ac58d0fc6
Vlad <19vlad_son90@mail.ru>
2016-05-31 19:14:11 +0300

    JavaRushHomeWork

---
+++ src/com/javarush/test/level37/lesson04/big01/female/FemaleFactory.java
@@ -0,0 +1,17 @@
+package com.javarush.test.level37.lesson04.big01.female;
+
+import com.javarush.test.level37.lesson04.big01.Human;
+
+/**
+ * Created by Влад on 31.05.2016.
+ */
+public class FemaleFactory {
+    public Human getPerson(int age) {
+        if (KidGirl.MAX_AGE >= age)
+            return new KidGirl();
+        else if (TeenGirl.MAX_AGE >= age)
+            return new TeenGirl();
+        else
+            return new Woman();
+    }
+}
