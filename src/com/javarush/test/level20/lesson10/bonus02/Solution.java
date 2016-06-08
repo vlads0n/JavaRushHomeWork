e3f6fa516c9d334b0314d7c0e3e4b41ec3b50f13
Vlad <19vlad_son90@mail.ru>
2016-06-08 20:15:52 +0300

    JavaRushHomeWork

--- src/com/javarush/test/level20/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level20/lesson10/bonus02/Solution.java
@@ -13,21 +13,52 @@ package com.javarush.test.level20.lesson10.bonus02;
 public class Solution {
     public static void main(String[] args) {
         byte[][] a = new byte[][]{
-                {1, 1, 0, 0},
-                {1, 1, 0, 0},
-                {1, 1, 0, 0},
-                {1, 1, 0, 1}
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1},
+                {1, 1, 0, 0, 0},
+                {1, 1, 0, 0, 1},
+                {0, 0, 0, 0, 1}
         };
         int count = getRectangleCount(a);
         System.out.println("count = " + count + ". Должно быть 2");
     }
 
     public static int getRectangleCount(byte[][] a) {
+        int downLine = 0;
+        int downRow = 0;
+        int upLine;
+        int upRow;
         int count = 0;
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if(a[i][j] == 1)
+        for (int row = 0; row < a.length; row++) {
+            for (int line = 0; line < a.length; line++) {
+                if (a[line][row] == 1) {
+                    upLine = line;
+                    upRow = row;
+                    for (int k = upLine; k < a.length; ) {
+                        downLine = k;
+                        if (a[k][upRow] == 1)
+                            k++;
+                        else if (a[k][upRow] == 0) {
+                            downLine = k - 1;
+                            break;
+                        }
+                    }
+                    for (int k = upRow; k < a.length; ) {
+                        downRow = k;
+                        if (a[downLine][k] != 0)
+                            k++;
+                        else if (a[downLine][k] == 0) {
+                            downRow = k - 1;
+                            break;
+                        }
+                    }
+                    for (int i = upLine; i < downLine + 1; i++) {
+                        for (int j = upRow; j < downRow + 1; j++) {
+                            a[i][j] = 0;
+                        }
+                    }
                     count++;
+                }
             }
         }
         return count;
