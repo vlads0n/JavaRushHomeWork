b1a3319883979055ca440e225d5298eb421cb626
Vlad <19vlad_son90@mail.ru>
2016-06-13 21:13:34 +0300

    JavaRushHomeWork

--- src/com/javarush/test/level34/lesson08/bonus01/Cache.java
+++ src/com/javarush/test/level34/lesson08/bonus01/Cache.java
@@ -1,18 +1,31 @@
 package com.javarush.test.level34.lesson08.bonus01;
 
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 public class Cache<K, V> {
-    private Map<K, V> cache = null;   //TODO add your code here
+    private Map<K, V> cache = new WeakHashMap<>();
 
     public V getByKey(K key, Class<V> clazz) throws Exception {
-        //TODO add your code here
-        return null;
+        if (cache.get(key) == null) {
+            V object = clazz.getConstructor(key.getClass()).newInstance(key);
+            cache.put(key, object);
+        }
+        return cache.get(key);
     }
 
     public boolean put(V obj) {
-        //TODO add your code here
-        return false;
+        try {
+            Method getKey = obj.getClass().getDeclaredMethod("getKey");
+            getKey.setAccessible(true);
+            K key = (K) getKey.invoke(obj);
+            cache.put(key, obj);
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
 
     public int size() {
