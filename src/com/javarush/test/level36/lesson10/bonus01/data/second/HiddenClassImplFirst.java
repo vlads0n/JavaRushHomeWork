afa666b82ea1584588ac4b1ce7f270131ab1d52b
vlad_son <19vlad_son90@bk.ru>
2016-06-22 10:22:03 +0300

    JavaRushHomeWork

--- src/com/javarush/test/level36/lesson10/bonus01/data/second/HiddenClassImplFirst.java
+++
@@ -1,6 +0,0 @@
-package com.javarush.test.level36.lesson10.bonus01.data.second;
-
-import com.javarush.test.level36.lesson10.bonus01.HiddenClass;
-
-public class HiddenClassImplFirst implements HiddenClass {
-}
