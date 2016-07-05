a0e88fba9bb3cdabb0a82ee2f3da88ca9661e3a1
Vlad <19vlad_son90@mail.ru>
2016-07-05 21:54:48 +0300

    JavaRushHomeWork

--- src/com/javarush/test/level27/lesson15/big01/Restaurant.java
+++ src/com/javarush/test/level27/lesson15/big01/Restaurant.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level27.lesson15.big01;
 
+
 import com.javarush.test.level27.lesson15.big01.kitchen.Cook;
 import com.javarush.test.level27.lesson15.big01.kitchen.Waitor;
 import com.javarush.test.level27.lesson15.big01.statistic.StatisticEventManager;
@@ -7,7 +8,6 @@ import com.javarush.test.level27.lesson15.big01.statistic.StatisticEventManager;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
 
 /**
  * Created by Влад on 12.11.2015.
@@ -30,8 +30,9 @@ public class Restaurant
         cook2.addObserver(waitor);
 
         List<Tablet> tablets = new ArrayList<>();
+        Tablet tablet;
         for (int i = 0; i < 5; i++) {
-            Tablet tablet = new Tablet(i);
+            tablet = new Tablet(i);
             tablets.add(tablet);
             tablet.addObserver(cook1);
             tablet.addObserver(cook2);
