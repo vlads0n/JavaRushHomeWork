7326297b577bc239db5dd354800b2436ee6f0b6c
Vlad <19vlad_son90@mail.ru>
2016-07-05 19:23:38 +0300

    JavaRushHomeWork

---
+++ src/com/javarush/test/level38/lesson10/home02/Revision.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level38.lesson10.home02;
+
+public @interface Revision {
+    int revision();
+    Date date();
+    Author[] authors() default {};
+    String comment() default "";
+}
