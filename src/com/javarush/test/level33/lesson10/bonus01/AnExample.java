4d972166d3fbbdb77076129870b42f111808ce06
vlad_son <19vlad_son90@bk.ru>
2016-07-13 16:53:14 +0300

    JavaRushHomeWork

---
+++ src/com/javarush/test/level33/lesson10/bonus01/AnExample.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level33.lesson10.bonus01;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Created by Vinnik on 13.07.2016.
+ */
+@XmlType(name = "anExample")
+@XmlRootElement
+public class AnExample {
+    public String[] needCDATA = new String[]{"need CDATA because of < and >", ""};
+}
--- src/com/javarush/test/level33/lesson10/bonus01/Solution.java
+++ src/com/javarush/test/level33/lesson10/bonus01/Solution.java
@@ -1,5 +1,10 @@
 package com.javarush.test.level33.lesson10.bonus01;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
 /* Комментарий внутри xml
 Реализовать метод toXmlWithComment, который должен возвращать строку - xml представление объекта obj.
 В строке перед каждым тэгом tagName должен быть вставлен комментарий comment.
@@ -21,6 +26,44 @@ package com.javarush.test.level33.lesson10.bonus01;
 */
 public class Solution {
     public static String toXmlWithComment(Object obj, String tagName, String comment) {
-        return null;
+        String resultXML = null;
+        try {
+            StringWriter stringWriter = new StringWriter();
+            JAXBContext context = JAXBContext.newInstance(obj.getClass());
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(obj, stringWriter);
+
+            resultXML = stringWriter.toString();
+            String openTag = "<" + tagName;
+            String closingTag = "</" + tagName;
+            int indexOfOpenTag = resultXML.indexOf(openTag);
+            int indexOfClosingTag = resultXML.indexOf(closingTag);
+            int tagLength = tagName.length();
+            String commentary = "\t<!--" + comment + "-->\n\t";
+
+            while (indexOfOpenTag != -1 && indexOfClosingTag != -1) {
+                if (indexOfOpenTag < indexOfClosingTag) {
+                    resultXML = resultXML.substring(0, indexOfOpenTag - 1) + commentary + resultXML.substring(indexOfOpenTag - 1);
+                    tagLength += commentary.length();
+                    indexOfOpenTag = resultXML.indexOf(tagName, tagLength + indexOfOpenTag);
+                    indexOfClosingTag = resultXML.indexOf(tagName, tagLength + indexOfClosingTag);
+                }
+                else if (indexOfOpenTag > indexOfClosingTag)
+                {
+                    resultXML = resultXML.substring(0, indexOfOpenTag - 2) + commentary + resultXML.substring(indexOfOpenTag - 2);
+                    tagLength += commentary.length();
+                    indexOfOpenTag = resultXML.indexOf(tagName, tagLength + indexOfOpenTag);
+                    indexOfClosingTag = resultXML.indexOf(tagName, tagLength + indexOfClosingTag);
+                }
+            }
+        } catch (JAXBException e) {}
+
+        return resultXML;
+    }
+
+    public static void main(String[] args) {
+       String result = toXmlWithComment(new AnExample(), "needCDATA", "it's a comment - <needCDATA>");
+        System.out.println(result);
     }
 }
