818ea3b1cf9e54ad20b7b55ba761cd85dc948fa3
vlad_son <19vlad_son90@bk.ru>
2016-07-19 20:10:04 +0300

    JavaRushHomeWork

---
+++ src/com/javarush/test/test/Frequency.java
@@ -0,0 +1,9 @@
+package com.javarush.test.test;
+
+/**
+ * Created by Vinnik on 19.07.2016.
+ */
+public enum Frequency {
+    ONE_HUNDRED_HZ,
+    FIVE_HUNDRED_HZ
+}
---
+++ src/com/javarush/test/test/Generator.java
@@ -0,0 +1,16 @@
+package com.javarush.test.test;
+
+/**
+ * Created by Vinnik on 19.07.2016.
+ */
+public class Generator {
+    private Frequency frequency;
+
+    public Generator(Frequency frequency) {
+        this.frequency = frequency;
+    }
+
+    public Frequency getFrequency() {
+        return frequency;
+    }
+}
